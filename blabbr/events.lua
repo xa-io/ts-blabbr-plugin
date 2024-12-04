8250b52cf67f699e9a929f0ce36a37dcb68c490b
ѪѦ <151899564+xa-io@users.noreply.github.com>
2024-12-04 11:52:09 -0800

    Implemented Timestamps

--- blabbr/events.lua
+++ blabbr/events.lua
@@ -9,7 +9,15 @@ local function onTalkStatusChangeEvent(serverConnectionHandlerID, status, isRece
 	if status == ts3defs.TalkStatus.STATUS_TALKING then
 		local displayName, error = ts3.getClientDisplayName(serverConnectionHandlerID, clientID)
 		local uniqueIdentifier, error = ts3.getClientVariableAsString(serverConnectionHandlerID, clientID, ts3defs.ClientProperties.CLIENT_UNIQUE_IDENTIFIER)
-		ts3.printMessage(serverConnectionHandlerID, ":: [URL=client://" .. clientID .. "/" .. uniqueIdentifier .. "][color=green]" .. displayName .. "[/color][/URL]", 1)
+
+		-- Get the current time in HH:MM:SS format
+		local timestamp = os.date("<%H:%M:%S>")
+		
+		-- Construct the message with the timestamp
+		local message = timestamp .. " [URL=client://" .. clientID .. "/" .. uniqueIdentifier .. "][color=green]" .. displayName .. "[/color][/URL]"
+
+		-- Print the message to the TeamSpeak chat window
+		ts3.printMessage(serverConnectionHandlerID, ":: " .. message, 1)
 	end    
 end
 
